f71afb5322a64e28babd478c98cbb2ea80766a78
diana <zS22017039@estudiantes.uv.mx>
2025-05-21 01:06:28 -0600

    conecxión a la base de datos

---
+++ src/Servidor/baseDatos/ConexionBD.java
@@ -0,0 +1,34 @@
+package Servidor.baseDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+
+    private Connection conexion;
+
+    public ConexionBD() throws SQLException {
+        String url = "jdbc:mysql://127.0.0.1:3306/sistema_medico";
+        String usuario = "root";
+        String password = "";
+
+        conexion = DriverManager.getConnection(url, usuario, password);
+        System.out.println("Conexión a la base de datos establecida.");
+    }
+
+    public Connection getConexion() {
+        return conexion;
+    }
+
+    public void cerrarConexion() {
+        if (conexion != null) {
+            try {
+                conexion.close();
+                System.out.println("Conexión cerrada.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
